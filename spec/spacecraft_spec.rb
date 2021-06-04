248a0dae834c67b9537f83be98b382cfe022e912
Phillip Strom <phillip.strom@gmail.com>
2021-06-04 12:13:26 -0600

    Create spacecraft spec

---
+++ spec/spacecraft_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/spacecraft'
+
+RSpec.describe 'Spacecraft' do
+  it 'is a spacecraft' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+     expect(daedalus).to be_instance_of(Spacecraft)
+  end
+  it 'has attributes' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus.name).to eq('Daedalus')
+    expect(daedalus.fuel).to eq(400)
+  end
+end
