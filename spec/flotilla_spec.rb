82ad858462fbd7a69b97dc4531b37aad00d5f010
Phillip Strom <phillip.strom@gmail.com>
2021-06-04 13:10:53 -0600

    Add personnel by ship spec

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -71,4 +71,46 @@ RSpec.describe 'Flotilla' do
     expected = [polly]
     expect(@seventh_flotilla.recommend_personnel(odyssey)).to eq(expected)
   end
+
+  it "can list personell by ship" do
+    @daedalus.add_requirement({astrophysics: 6})
+    @daedalus.add_requirement({quantum_mechanics: 3})
+
+    odyssey = Spacecraft.new({name: 'Odyssey', fuel: 300})
+    odyssey.add_requirement({operations: 6})
+    odyssey.add_requirement({maintenance: 3})
+
+    kathy = Person.new('Kathy Chan', 10)
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+
+    polly = Person.new('Polly Parker', 8)
+    polly.add_specialty(:operations)
+    polly.add_specialty(:maintenance)
+
+    rover = Person.new('Rover Henriette', 1)
+    rover.add_specialty(:operations)
+    rover.add_specialty(:maintenance)
+
+    sampson = Person.new('Sampson Edwards', 7)
+    sampson.add_specialty(:astrophysics)
+    sampson.add_specialty(:quantum_mechanics)
+
+    @seventh_flotilla.add_personnel(kathy)
+    @seventh_flotilla.add_personnel(polly)
+    @seventh_flotilla.add_personnel(rover)
+    @seventh_flotilla.add_personnel(sampson)
+
+    @seventh_flotilla.ships eq([])
+    @seventh_flotilla.add_ship(@daedalus)
+    @seventh_flotilla.add_ship(odyssey)
+    expect(@seventh_flotilla.ships).to eq([@daedalus, odyssey])
+
+    expected = {
+      @daedalus => [],
+      odyssey => []
+    }
+
+    expect(@seventh_flotilla.personnel_by_ship).to eq(expected)
+  end
 end
