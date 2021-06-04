ccd4e1309009187f9dc0bed72c4e51e58eaf6bbf
Phillip Strom <phillip.strom@gmail.com>
2021-06-04 12:19:20 -0600

    Create person test

---
+++ spec/person_spec.rb
@@ -0,0 +1,24 @@
+require './lib/person'
+
+
+RSpec.describe 'Person' do
+  it 'can be instantiated' do
+    kathy = Person.new('Kathy Chan', 10)
+
+    expect(kathy).to be_instance_of(Person)
+  end
+  it 'has attributes' do
+    kathy = Person.new('Kathy Chan', 10)
+
+    expect(kathy.name).to eq('Kathy Chan')
+    expect(kathy.experience).to eq(10)
+    expect(kathy.specialties).to eq([])
+  end
+  it "can add specialties" do
+    kathy = Person.new('Kathy Chan', 10)
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+
+    expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+  end
+end
