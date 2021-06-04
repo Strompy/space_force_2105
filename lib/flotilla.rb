e1b54ad4b8a6d98fbfe53500f3ad314fbafeb030
Phillip Strom <phillip.strom@gmail.com>
2021-06-04 13:04:07 -0600

    Create flotilla class

---
+++ lib/flotilla.rb
@@ -0,0 +1,28 @@
+class Flotilla
+  attr_reader :name, :personnel, :ships
+
+  def initialize(details)
+    @name = details[:designation]
+    @personnel = []
+    @ships = []
+  end
+
+  def add_ship(ship)
+    @ships.push(ship)
+  end
+
+  def add_personnel(person)
+    @personnel.push(person)
+  end
+
+  def recommend_personnel(ship)
+    personnel.find_all do |person|
+      next if person.specialties.empty?
+
+      ship.requirements.all? do |req|
+        area, exp = req.first
+        person.experience >= exp && person.specialties.include?(area)
+      end
+    end
+  end
+end
