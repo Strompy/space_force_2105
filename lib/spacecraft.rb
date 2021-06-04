6f81f73f32261e6cc7d14de587b97db0cdda7d96
Phillip Strom <phillip.strom@gmail.com>
2021-06-04 12:25:28 -0600

    Create add requirements method

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,8 +1,13 @@
 class Spacecraft
-  attr_reader :name, :fuel
+  attr_reader :name, :fuel, :requirements
 
   def initialize(details)
     @name = details[:name]
     @fuel = details[:fuel]
+    @requirements = []
+  end
+
+  def add_requirement(req)
+    @requirements.push(req)
   end
 end
